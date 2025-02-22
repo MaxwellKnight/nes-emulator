27857b26431928e7dc05897f76775679eed8d14d
Maxwell Knight <maxwell.knight98@gmail.com>
2025-02-22 22:35:12 +0200

    removed memory class and combined it with bus class

--- include/bus.h
+++ include/bus.h
@@ -1,17 +1,20 @@
 #pragma once
 #include "memory.h"
 #include "types.h"
+#include <array>
 
 namespace nes {
 
 class Bus {
 public:
   Bus();
-
+  static constexpr size_t MEMORY_SIZE = 65536; // 64KB
   void write(u16 address, u8 data);
+  void write_word(u16 address, u16 value);
   [[nodiscard]] u8 read(u16 address) const;
+  [[nodiscard]] u16 read_word(u16 address) const;
 
 private:
-  Memory memory;
+  std::array<u8, MEMORY_SIZE> memory{};
 };
 } // namespace nes
--- src/bus.cpp
+++ src/bus.cpp
@@ -2,15 +2,20 @@
 
 namespace nes {
 
-Bus::Bus() {
+Bus::Bus() {}
+void Bus::write(u16 address, u8 value) { memory[address] = value; }
+
+u8 Bus::read(u16 address) const { return memory[address]; }
+
+u16 Bus::read_word(u16 address) const {
+  u8 low = read(address);
+  u8 high = read(address + 1);
+  return static_cast<u16>(low) | (static_cast<u16>(high) << 8);
 }
 
-void Bus::write(u16 address, u8 data) {
-    memory.write(address, data);
-}
-
-u8 Bus::read(u16 address) const {
-    return memory.read(address);
+void Bus::write_word(u16 address, u16 value) {
+  write(address, value & 0xFF);
+  write(address + 1, (value >> 8) & 0xFF);
 }
 
 } // namespace nes
