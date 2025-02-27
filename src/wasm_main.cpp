73e97ef563a921b700283fdd1a59344055f22c75
Maxwell Knight <maxwell.knight98@gmail.com>
2025-02-28 01:14:54 +0200

    fixed cmake file for building tests

--- src/wasm_main.cpp
+++ src/wasm_main.cpp
@@ -11,18 +11,15 @@
 #define EMSCRIPTEN_EXPORT extern "C"
 #endif
 
-// Global instances that will be used by the debugger
 nes::Bus g_bus;
 nes::CPU g_cpu(g_bus);
 nes::Debugger g_debugger(g_cpu, g_bus);
 
-// Main initialization function
 EMSCRIPTEN_EXPORT int init() {
   g_cpu.reset();
   return 1;
 }
 
-// Main emulation loop that will be called by JavaScript
 EMSCRIPTEN_EXPORT void main_loop() {
   if (g_debugger.is_running()) {
     g_debugger.step();
@@ -33,9 +30,6 @@ EMSCRIPTEN_EXPORT void main_loop() {
 int main() {
   // Initialize everything
   init();
-
-  // We don't set up an animation frame callback here
+  // Not settting up an animation frame callback here
   // Instead, JavaScript will call main_loop() when needed
-
-  return 0;
 }
